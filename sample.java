0ba490f33922697d3af11a3cb22dc3bce3ebc712
API1203 <85011029+API1203@users.noreply.github.com>
2021-06-03 02:17:02 +0530

    Git to Bit

--- sample.java
+++ sample.java
@@ -5,7 +5,7 @@ public class sample {
   public static void main(String[] args) { 
    
     System.out.println("Hello Sample Code To Push To GitHub"):
-      System.out.println("Code Edited in GitHub to be Pushed to BitBucket"):
+      System.out.println("Code Edited in GitHub to be Pushed to BitBucket");
 
   } 
 }
