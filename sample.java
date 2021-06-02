c8e463e5526a25d7a8d2537c69b22d24c605e709
Tabiyul <tabiyul.mohammed@apisero.com>
2021-06-03 02:09:04 +0530

    GitHub Before Bit Push

--- sample.java
+++ sample.java
@@ -3,16 +3,7 @@ import java.util.ArrayList;
 
 public class sample { 
   public static void main(String[] args) { 
-    ArrayList<String> cars = new ArrayList<String>();
-    cars.add("Volvo");
-    cars.add("BMW");
-    cars.add("Ford");
-    cars.add("Maza");
-    cars.add("HELLO");
-    cars.add("From Github");
-cars.add("From Jenkin COmmit");
-cars.add("From Jenkin COmmt");
-cars.add("From Bit COmmt");
-    System.out.println(cars);
+   
+    System.out.println("Hello Sample Code To Push To GitHub
   } 
 }
