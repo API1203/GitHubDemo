3d226a26abc00ab40103fe59e6f3f725e0229abc
API1203 <85011029+API1203@users.noreply.github.com>
2021-06-03 02:14:46 +0530

    Git to Bit

--- sample.java
+++ sample.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 public class sample { 
   public static void main(String[] args) { 
    
-    System.out.println("Hello Sample Code To Push To GitHub
+    System.out.println("Hello Sample Code To Push To GitHub"):
+      System.out.println("Code Edited in GitHub to be Pushed to BitBucket"):
+
   } 
 }
